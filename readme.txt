d59c044e37d5e3dfdd7d853d95d3c4c110937e9d
mehulkaklotar <mehulkaklotar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 17:55:26 +0000

    Update to commit 66b60f1 from git@github.com:mehulkaklotar/woocommerce-custom-emails.git
    git-svn-id: https://plugins.svn.wordpress.org/woo-custom-emails/tags/1.2@1217466 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Woo Custom Emails ===
+Contributors: mehulkaklotar, utkarshpatel
+Tags: woocommerce, email, order, ecommerce, custom email template
+Requires at least: 4.1
+Tested up to: 4.2.4
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Woo Custom Emails is a plugin that helps you create custom emails for your WooCommerce Order.
+
+== Description ==
+
+Create your custom WooCommerce order emails with WooCommerce default email settings functionality.
+
+= Contributing to Woo Custom Emails =
+If you have a patch, or stumbled upon an issue with Woo Custom Emails core, you can contribute this back to the code. Submit your pull request or issue on [Github WooCustomEmails](https://github.com/mehulkaklotar/woo-custom-emails)
+
+== Installation ==
+
+= From your WordPress dashboard =
+
+1. Visit 'Plugins > Add New'
+2. Search for 'Woo Custom Emails'
+3. Activate Woo Custom Emails from your Plugins page.
+4. Go to WordPress Settings menu -> WC Emails.
+
+= From WordPress.org =
+
+1. Download Woo Custom Emails.
+2. Upload the 'woo-custom-emails' directory to your '/wp-content/plugins/' directory, using your favorite method (ftp, sftp, scp, etc...)
+3. Activate Woo Custom Emails from your Plugins page.
+4. Go to WordPress Settings menu -> WC Emails.
+
+== Screenshots ==
+
+1. Add/Edit custom emails
+2. View custom emails list
+3. WooCommerce email settings
+
+== Changelog ==
+
+= 1.2 =
+* Email subject field
+* Description with textarea field
+
+= 1.1 =
+* Unique id generation for email classes
+* Assets added
+
+= 1.0 =
+* Initial release
+* Custom WooCommerce order emails
+* WP Editor for email template
---
+++ woo-custom-emails.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Plugin Name: Woo Custom Emails
+ * Plugin URI: https://github.com/mehulkaklotar/woo-custom-emails
+ * Description: A woocommerce add on to support customize emails
+ * Version: 1.2
+ * Author: Mehul Kaklotar
+ * Author URI: http://mehulkaklotar.branded.me
+ * Requires at least: 4.1
+ * Tested up to: 4.2.4
+ *
+ * Text Domain: woo-custom-emails
+ *
+ * @package Woo_Custom_Emails
+ * @category Core
+ * @author mehulkaklotar
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'Woo_Custom_Emails' ) ) {
+
+	/**
+	 * Main Woo Custom Emails Class
+	 *
+	 * @class Woo_Custom_Emails
+	 * @version	0.1
+	 */
+	final class Woo_Custom_Emails {
+
+		/**
+		 * @var string
+		 */
+		public $version = '1.2';
+		/**
+		 * @var Woo_Custom_Emails The single instance of the class
+		 * @since 2.1
+		 */
+		protected static $_instance = null;
+
+		/**
+		 * Main Woo_Custom_Emails Instance
+		 *
+		 * Ensures only one instance of WooCommerce_Custom_Emails is loaded or can be loaded.
+		 *
+		 * @since 0.1
+		 * @static
+		 * @see woo_custom_emails()
+		 * @return Woo_Custom_Emails - Main instance
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+
+		/**
+		 * Cloning is forbidden.
+		 * @since 0.1
+		 */
+		public function __clone() {
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'woo-custom-emails' ), '0.1' );
+		}
+		/**
+		 * Unserializing instances of this class is forbidden.
+		 * @since 2.1
+		 */
+		public function __wakeup() {
+			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'woo-custom-emails' ), '0.1' );
+		}
+
+		/**
+		 * WooCommerce_Custom_Emails Constructor.
+		 */
+		public function __construct() {
+			$this->define_constants();
+			$this->init_hooks();
+		}
+
+		/**
+		 * Hook into actions and filters
+		 * @since  0.1
+		 */
+		private function init_hooks() {
+			add_action( 'init', array( $this, 'init' ) );
+		}
+
+		/**
+		 * Define WCE Constants
+		 */
+		private function define_constants() {
+			$this->define( 'WCEmails_PLUGIN_FILE', __FILE__ );
+			$this->define( 'WCEmails_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+			$this->define( 'WCEmails_VERSION', $this->version );
+			$this->define( 'WCEmails_TEXT_DOMAIN', 'wcemails' );
+			$this->define( 'WCEmails_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+			$this->define( 'WCEmails_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		}
+
+		/**
+		 * Define constant if not already set
+		 * @param  string $name
+		 * @param  string|bool $value
+		 */
+		private function define( $name, $value ) {
+			if ( ! defined( $name ) ) {
+				define( $name, $value );
+			}
+		}
+
+		/**
+		 * Include required core files used in admin and on the frontend.
+		 */
+		public function includes() {
+			include_once( 'admin/class-wcemails-admin.php' );
+		}
+
+		/**
+		 * Init WooCommerce when WordPress Initialises.
+		 */
+		public function init() {
+			$this->includes();
+		}
+
+		/**
+		 * Get the plugin url.
+		 * @return string
+		 */
+		public function plugin_url() {
+			return untrailingslashit( plugins_url( '/', __FILE__ ) );
+		}
+
+		/**
+		 * Get the plugin path.
+		 * @return string
+		 */
+		public function plugin_path() {
+			return untrailingslashit( plugin_dir_path( __FILE__ ) );
+		}
+
+	}
+
+}
+
+/**
+ * Returns the main instance of woo_custom_emails to prevent the need to use globals.
+ *
+ * @since  0.1
+ * @return Woo_Custom_Emails
+ */
+function woo_custom_emails() {
+	return Woo_Custom_Emails::instance();
+}
+woo_custom_emails();
